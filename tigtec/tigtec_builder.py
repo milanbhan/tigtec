33581cc98e7a5b33c53a7c1cd132a9fa90edf5a8
milanbhan <milan.bhan@ekimetrics.com>
2022-08-23 10:49:29 +0200

    pplxt add on

--- tigtec/tigtec_builder.py
+++ tigtec/tigtec_builder.py
@@ -9,6 +9,8 @@ from copy import deepcopy
 from tqdm.notebook import tqdm
 import string  
 import seaborn as sns
+import tqdm
+
 
 #PyTorch
 import torch
@@ -386,9 +388,34 @@ class tigtec:
             
         return(grammar_accuracy_list)
                     
-                 
-            
-            
-            
+    def perplexity(self, stride=64) :
+        from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+
+        device = "cuda"
+        model_id = "gpt2-large"
+        model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+        tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
         
+        text =  "\n\n".join(["\n\n".join(cf) for cf in self.cf_list])      
         
+        encodings = tokenizer(text, return_tensors="pt")
+        max_length = model.config.n_positions
+
+        nlls = []
+        for i in tqdm(range(0, encodings.input_ids.size(1), stride)):
+            begin_loc = max(i + stride - max_length, 0)
+            end_loc = min(i + stride, encodings.input_ids.size(1))
+            trg_len = end_loc - i  # may be different from stride on last loop
+            input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+            target_ids = input_ids.clone()
+            target_ids[:, :-trg_len] = -100
+
+            with torch.no_grad():
+                outputs = model(input_ids, labels=target_ids)
+                neg_log_likelihood = outputs[0] * trg_len
+
+            nlls.append(neg_log_likelihood)
+
+        ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+        
+        return(ppl)
