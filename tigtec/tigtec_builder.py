af562af17307fd33c6219c2061230fdfa8011833
milanbhan <milan.bhan@ekimetrics.com>
2022-10-28 18:38:48 +0200

    debug perplexity for calculability

--- tigtec/tigtec_builder.py
+++ tigtec/tigtec_builder.py
@@ -523,30 +523,33 @@ class tigtec:
         model_id = "gpt2-large"
         model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
         tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+        ppl_list = []
+        for idx in range(len(self.cf_list)) :
         
-        text =  " \n\n ".join([" \n\n ".join(cf) for cf in self.cf_list])
+            text =  " \n\n ".join([" \n\n ".join(cf) for cf in self.cf_list[idx]])
+            
+            encodings = tokenizer(text, return_tensors="pt")
+            max_length = model.config.n_positions
+
+            nlls = []
+            for i in range(0, encodings.input_ids.size(1), stride):
+                begin_loc = max(i + stride - max_length, 0)
+                end_loc = min(i + stride, encodings.input_ids.size(1))
+                trg_len = end_loc - i  # may be different from stride on last loop
+                input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+                target_ids = input_ids.clone()
+                target_ids[:, :-trg_len] = -100
+
+                with torch.no_grad():
+                    outputs = model(input_ids, labels=target_ids)
+                    neg_log_likelihood = outputs[0] * trg_len
+
+                nlls.append(neg_log_likelihood)
+
+            ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+            ppl_list.append(ppl)
         
-        encodings = tokenizer(text, return_tensors="pt")
-        max_length = model.config.n_positions
-
-        nlls = []
-        for i in range(0, encodings.input_ids.size(1), stride):
-            begin_loc = max(i + stride - max_length, 0)
-            end_loc = min(i + stride, encodings.input_ids.size(1))
-            trg_len = end_loc - i  # may be different from stride on last loop
-            input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-            target_ids = input_ids.clone()
-            target_ids[:, :-trg_len] = -100
-
-            with torch.no_grad():
-                outputs = model(input_ids, labels=target_ids)
-                neg_log_likelihood = outputs[0] * trg_len
-
-            nlls.append(neg_log_likelihood)
-
-        ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
-        
-        return(ppl)
+        return(ppl_list)
     
     def loss(self, stride = 64):
         bleu_score = np.mean(self.bleu_score())
